c17184b2cd46c5698608702a919b4318b5cdc94b
ashwinnagelwar <ashwinnagelwar@gmail.com>
2025-02-07 14:55:18 +0530

    code

---
+++ src/main/java/com/example/demo/KuberenetesFirstDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuberenetesFirstDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KuberenetesFirstDemoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class DemoController {
+
+	@GetMapping("/hello")
+	public String greetHello()
+	{
+		return "HI, This is my first Kubernetes Project...!";
+	}
+}
